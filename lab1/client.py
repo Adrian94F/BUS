775a6209823462fb00ae2b70d9e7cbb3ae9b0e80
Adrian <209865@student.pwr.edu.pl>
2017-10-10 20:42:53 +0200

    simple echo

---
+++ lab1/client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 2580)
+print('connecting to {}'.format(server_address))
+sock.connect(server_address)
+
+try:
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print('sending "{}"'.format(message))
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+    
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print('received "{}"'.format(data))
+finally:
+    print('closing socket')
+    sock.close()
