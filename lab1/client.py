de2c8bfbf0a7c6174408bb431ff90910e6241ec3
Frydmanski Adrian <adrian.frydmanski@nokia.com>
2017-10-11 18:46:32 +0200

    works with 1 client, crashes when 2nd client connects

--- lab1/client.py
+++ lab1/client.py
@@ -5,6 +5,15 @@ import socket
 import sys
 import json
 import threading
+import time
+
+if len(sys.argv) != 3 and len(sys.argv) != 4:
+    print('usage: client.py server_address port_number')
+    sys.exit()
+
+addr = sys.argv[1]
+port = int(sys.argv[2])
+encryption = sys.argv[3] if len(sys.argv) == 4 else 'none'
 
 buffer_size = 1024
 
@@ -14,8 +23,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 name = raw_input('type your name: ')
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 2580)
-print('connecting to {}'.format(server_address))
+server_address = (addr, 2580)
+print('connecting to {} with encryption set to \'{}\''.format(server_address, encryption))
 sock.connect(server_address)
 
 class OutputThr(threading.Thread):
@@ -41,11 +50,15 @@ try:
        print('error: unable to start thread')
 
     # Start input reading
+    counter = 1
     while True:
-        message = raw_input()
+        message = 'client{} - {}'.format(name, counter) # raw_input()
         # JSON magic...
-        sock.sendall(message)
+        sock.send(message)
+        counter += 1
+        time.sleep(2)
 
 except KeyboardInterrupt:
     mythread._Thread__stop()
+    sock.close()
     print(' time to say good bye!')
--- lab1/server.py
+++ lab1/server.py
@@ -4,46 +4,85 @@
 import socket
 import sys
 import json
+from threading import Thread
+from SocketServer import ThreadingMixIn 
+
+if len(sys.argv) != 2:
+    print('usage: server.py port_number')
+    sys.exit() 
+
+port = int(sys.argv[1])
 
 buffer_size = 1024
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the port
-server_address = ('localhost', 2580)
+server_address = ('localhost', port)
 print('starting up on {}'.format(server_address))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 sock.bind(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
+threads = []
 
 # Hardcoded
 g = 123
 p = 456
 
+class ClientHandler(Thread):
+
+    def __init__(self,ip,port): 
+        Thread.__init__(self) 
+        self.ip = ip 
+        self.port = port 
+        print "[+] New server socket thread started for {}:{}".format(ip, str(port))
+ 
+    def run(self): 
+        while True : 
+            data = conn.recv(2048) 
+            print('[r] from {}:{} \'{}\''.format(ip, str(port), data))
+            if data:
+                print('[s] to {}:{} \'{}\''.format(ip, str(port), data))
+                conn.send(data)
+            else:
+                print('[-] no more data from {}:{}'.format(ip, str(port)))
+                break
+    # def run(self):
+    #     while True:
+    #         data = connection.recv(buffer_size)
+    #         print(data)
+
 try:
     while True:
-    # Wait for a connection
-        print('waiting for a connection')
-        connection, client_address = sock.accept()
+        # Listen for incoming connections
+        sock.listen(1)
 
-        try:
-            print('connection from {}'.format(client_address))
+        (conn, (ip,port)) = sock.accept() 
+        newthread = ClientHandler(ip,port) 
+        newthread.start() 
+        threads.append(newthread) 
 
-            # Receive the data in small chunks and retransmit it
-            while True:
-                data = connection.recv(buffer_size)
-                print('received "{}"'.format(data))
-                if data:
-                    print('sending data back to the client')
-                    connection.sendall(data)
-                else:
-                    print('no more data from'.format(client_address))
-                    break
-        finally:
-            # Clean up the connection
-            connection.close()
+        # connection, client_address = sock.accept()
+
+        # try:
+        #     print('connection from {}'.format(client_address))
+
+        #     # Receive the data in small chunks and retransmit it
+        #     while True:
+        #         data = connection.recv(buffer_size)
+        #         print('received "{}"'.format(data))
+        #         if data:
+        #             print('sending data back to the client')
+        #             connection.sendall(data)
+        #         else:
+        #             print('no more data from'.format(client_address))
+        #             break
+        # finally:
+        #     # Clean up the connection
+        #     connection.close()
+
+        for t in threads: 
+            if not t.isAlive():
+                t.join() 
 
 except KeyboardInterrupt:
     print(' time to say good bye!')
