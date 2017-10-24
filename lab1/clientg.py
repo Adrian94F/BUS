0f616062a5b51310912a301626fdaca803994c7c
Adrian <209865@student.pwr.edu.pl>
2017-10-24 19:19:28 +0200

    connecting to server

--- lab1/clientg.py
+++ lab1/clientg.py
@@ -5,7 +5,20 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
 import socket
+import re
+from message import *
 from thread import *
+import socket
+import sys
+import json
+import threading
+from threading import Thread
+import time
+from message import *
+from random import randint
+import base64
+from time import sleep
+import re
 
 
 def app_version():
@@ -19,7 +32,7 @@ def msg_box(title, data):
 
 def update_list(self, data):
     self.listWidget.addItem(data)
-    print "\a"
+    print('\a')
 
 
 try:
@@ -30,41 +43,39 @@ except AttributeError:
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-
-
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(662, 448)
+        MainWindow.resize(845, 435)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.frame = QFrame(self.centralwidget)
-        self.frame.setGeometry(QRect(10, 10, 651, 41))
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        self.label = QLabel(self.frame)
-        self.label.setGeometry(QRect(10, 10, 131, 21))
-        self.label.setObjectName(_fromUtf8("label"))
-        self.lineEdit = QLineEdit(self.frame)
-        self.lineEdit.setGeometry(QRect(90, 10, 131, 21))
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
-        self.label_2 = QLabel(self.frame)
-        self.label_2.setGeometry(QRect(230, 10, 131, 21))
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.lineEdit_2 = QLineEdit(self.frame)
-        self.lineEdit_2.setGeometry(QRect(270, 10, 71, 21))
-        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
-        self.label_3 = QLabel(self.frame)
-        self.label_3.setGeometry(QRect(350, 10, 81, 21))
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.lineEdit_3 = QLineEdit(self.frame)
-        self.lineEdit_3.setGeometry(QRect(435, 10, 51, 21))
-        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
-        self.label_4 = QLabel(self.frame)
-        self.label_4.setGeometry(QRect(500, 10, 41, 21))
-        self.label_4.setObjectName(_fromUtf8("label_4"))
-        self.lineEdit_4 = QLineEdit(self.frame)
-        self.lineEdit_4.setGeometry(QRect(540, 10, 90, 21))
-        self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
+        self.frame_top_bar = QFrame(self.centralwidget)
+        self.frame_top_bar.setGeometry(QRect(10, 10, 825, 41))
+        self.frame_top_bar.setFrameShape(QFrame.StyledPanel)
+        self.frame_top_bar.setFrameShadow(QFrame.Raised)
+        self.frame_top_bar.setObjectName(_fromUtf8("frame_top_bar"))
+        self.label_ip = QLabel(self.frame_top_bar)
+        self.label_ip.setGeometry(QRect(10, 10, 131, 21))
+        self.label_ip.setObjectName(_fromUtf8("label_ip"))
+        self.lineEdit_ip = QLineEdit(self.frame_top_bar)
+        self.lineEdit_ip.setGeometry(QRect(90, 10, 131, 21))
+        self.lineEdit_ip.setObjectName(_fromUtf8("lineEdit_ip"))
+        self.label_port = QLabel(self.frame_top_bar)
+        self.label_port.setGeometry(QRect(230, 10, 131, 21))
+        self.label_port.setObjectName(_fromUtf8("label_port"))
+        self.lineEdit_port = QLineEdit(self.frame_top_bar)
+        self.lineEdit_port.setGeometry(QRect(270, 10, 71, 21))
+        self.lineEdit_port.setObjectName(_fromUtf8("lineEdit_port"))
+        self.label_encryption = QLabel(self.frame_top_bar)
+        self.label_encryption.setGeometry(QRect(350, 10, 81, 21))
+        self.label_encryption.setObjectName(_fromUtf8("label_encryption"))
+        self.lineEdit_encryption = QLineEdit(self.frame_top_bar)
+        self.lineEdit_encryption.setGeometry(QRect(435, 10, 51, 21))
+        self.lineEdit_encryption.setObjectName(_fromUtf8("lineEdit_encryption"))
+        self.label_nick = QLabel(self.frame_top_bar)
+        self.label_nick.setGeometry(QRect(500, 10, 41, 21))
+        self.label_nick.setObjectName(_fromUtf8("label_4"))
+        self.lineEdit_nick = QLineEdit(self.frame_top_bar)
+        self.lineEdit_nick.setGeometry(QRect(540, 10, 90, 21))
+        self.lineEdit_nick.setObjectName(_fromUtf8("lineEdit_nick"))
         self.frame_2 = QFrame(self.centralwidget)
         self.frame_2.setGeometry(QRect(10, 60, 301, 321))
         self.frame_2.setFrameShape(QFrame.StyledPanel)
@@ -73,35 +84,45 @@ class Ui_MainWindow(object):
         self.textEdit = QTextEdit(self.frame_2)
         self.textEdit.setGeometry(QRect(10, 10, 281, 261))
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
-        self.pushButton_3 = QPushButton(self.frame_2)
-        self.pushButton_3.setGeometry(QRect(10, 280, 171, 31))
-        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.pushButton_send = QPushButton(self.frame_2)
+        self.pushButton_send.setGeometry(QRect(10, 280, 170, 31))
+        self.pushButton_send.setObjectName(_fromUtf8("pushButton_send"))
 
 
         #############################################################
         # Executes When The Send Message Button Is Clicked
-        self.pushButton_3.clicked.connect(self.client_send_message)
+        self.pushButton_send.clicked.connect(self.client_send_message)
+        ############################################################
+
+        self.pushButton_connect = QPushButton(self.frame_top_bar)
+        self.pushButton_connect.setGeometry(QRect(640, 5, 171, 31))
+        self.pushButton_connect.setObjectName(_fromUtf8("pushButton_connect"))
+
+
+        #############################################################
+        # Executes When The Connect  Button Is Clicked
+        self.pushButton_connect.clicked.connect(self.connect_with_server)
         ############################################################
 
 
-        self.pushButton_4 = QPushButton(self.frame_2)
-        self.pushButton_4.setGeometry(QRect(190, 280, 93, 31))
-        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.pushButton_clear = QPushButton(self.frame_2)
+        self.pushButton_clear.setGeometry(QRect(190, 280, 93, 31))
+        self.pushButton_clear.setObjectName(_fromUtf8("pushButton_clear"))
 
 
         #############################################################
         # Executes When The Clear Logs Button Is Clicked
-        self.pushButton_4.clicked.connect(self.clear_logs)
+        self.pushButton_clear.clicked.connect(self.clear_logs)
         ##############################################################
 
 
         self.frame_3 = QFrame(self.centralwidget)
-        self.frame_3.setGeometry(QRect(320, 60, 331, 321))
+        self.frame_3.setGeometry(QRect(320, 60, 515, 321))
         self.frame_3.setFrameShape(QFrame.StyledPanel)
         self.frame_3.setFrameShadow(QFrame.Raised)
         self.frame_3.setObjectName(_fromUtf8("frame_3"))
         self.listWidget = QListWidget(self.frame_3)
-        self.listWidget.setGeometry(QRect(10, 10, 311, 301))
+        self.listWidget.setGeometry(QRect(10, 10, 496, 301))
         self.listWidget.setObjectName(_fromUtf8("listWidget"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
@@ -113,24 +134,24 @@ class Ui_MainWindow(object):
         self.menuAction = QMenu(self.menubar)
         self.menuAction.setObjectName(_fromUtf8("menuAction"))
         MainWindow.setMenuBar(self.menubar)
-        self.actionExit = QAction(MainWindow)
-        self.actionExit.setObjectName(_fromUtf8("actionExit"))
+        self.actionExit_version = QAction(MainWindow)
+        self.actionExit_version.setObjectName(_fromUtf8("actionExit_version"))
 
         #######################################################
         # Executes When The SubMenu Item Version Is Clicked
-        self.actionExit.triggered.connect(app_version)
+        self.actionExit_version.triggered.connect(app_version)
         #######################################################
 
-        self.actionExit_2 = QAction(MainWindow)
-        self.actionExit_2.setObjectName(_fromUtf8("actionExit_2"))
+        self.actionExit_exit = QAction(MainWindow)
+        self.actionExit_exit.setObjectName(_fromUtf8("actionExit_exit"))
 
         #######################################################
         # Executes When The SubMenu Item Exit Is Clicked
-        self.actionExit_2.triggered.connect(qApp.quit)
+        self.actionExit_exit.triggered.connect(qApp.quit)
         #######################################################
 
-        self.menuAction.addAction(self.actionExit)
-        self.menuAction.addAction(self.actionExit_2)
+        self.menuAction.addAction(self.actionExit_version)
+        self.menuAction.addAction(self.actionExit_exit)
         self.menubar.addAction(self.menuAction.menuAction())
 
         self.retranslateUi(MainWindow)
@@ -140,58 +161,109 @@ class Ui_MainWindow(object):
         self.listWidget.clear()
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QApplication.translate("MainWindow",
-                                                         "JSONmessenger", None, QApplication.UnicodeUTF8))
-        self.label.setText(QApplication.translate("MainWindow", "IP Address:",
-                                                  None, QApplication.UnicodeUTF8))
-        self.label_2.setText(QApplication.translate("MainWindow", "Port: ",
-                                                    None, QApplication.UnicodeUTF8))
-        self.label_3.setText(QApplication.translate("MainWindow", "Encryption: ",
-                                                    None, QApplication.UnicodeUTF8))
-        self.label_4.setText(QApplication.translate("MainWindow", "Nick: ",
-                                                    None, QApplication.UnicodeUTF8))
-        self.pushButton_3.setText(QApplication.translate("MainWindow",
-                                                         "Send Message", None, QApplication.UnicodeUTF8))
-        self.pushButton_4.setText(QApplication.translate("MainWindow",
-                                                         "Clear Logs", None, QApplication.UnicodeUTF8))
-        self.menuAction.setTitle(QApplication.translate("MainWindow",
-                                                        "Menu Actions", None, QApplication.UnicodeUTF8))
-        self.actionExit.setText(QApplication.translate("MainWindow",
-                                                       "Version", None, QApplication.UnicodeUTF8))
-        self.actionExit_2.setText(QApplication.translate("MainWindow",
-                                                         "Exit", None, QApplication.UnicodeUTF8))
-        self.lineEdit.setText('localhost')
-        self.lineEdit_2.setText('2580')
-        self.lineEdit_3.setText('none')
-        self.lineEdit_4.setText('name')
+        MainWindow.setWindowTitle(QApplication.translate("MainWindow", "JSONmessenger", None, QApplication.UnicodeUTF8))
+        self.label_ip.setText(QApplication.translate("MainWindow", "IP Address:", None, QApplication.UnicodeUTF8))
+        self.label_port.setText(QApplication.translate("MainWindow", "Port: ", None, QApplication.UnicodeUTF8))
+        self.label_encryption.setText(QApplication.translate("MainWindow", "Encryption: ", None, QApplication.UnicodeUTF8))
+        self.label_nick.setText(QApplication.translate("MainWindow", "Nick: ", None, QApplication.UnicodeUTF8))
+        self.pushButton_send.setText(QApplication.translate("MainWindow", "Send Message", None, QApplication.UnicodeUTF8))
+        self.pushButton_connect.setText(QApplication.translate("MainWindow", "Connect", None, QApplication.UnicodeUTF8))
+        self.pushButton_clear.setText(QApplication.translate("MainWindow", "Clear Logs", None, QApplication.UnicodeUTF8))
+        self.menuAction.setTitle(QApplication.translate("MainWindow", "Menu", None, QApplication.UnicodeUTF8))
+        self.actionExit_version.setText(QApplication.translate("MainWindow", "Version", None, QApplication.UnicodeUTF8))
+        self.actionExit_exit.setText(QApplication.translate("MainWindow", "Exit", None, QApplication.UnicodeUTF8))
+        self.lineEdit_ip.setText('localhost')
+        self.lineEdit_port.setText('2580')
+        self.lineEdit_encryption.setText('none')
+        self.lineEdit_nick.setText('name')
+
+    def connect_with_server(self):
+        self.addr = self.lineEdit_ip.text()
+        self.port = int(self.lineEdit_port.text())
+        self.encryption = self.lineEdit_encryption.text()
+        self.name = self.lineEdit_nick.text()
+
+        buffer_size = 1024
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (self.addr, self.port)
+        print('[+] trying to connect to {} with encryption set to \'{}\''.format(server_address, self.encryption))
+        self.sock.connect(server_address)
+
+        alive = 1
+
+        # Request keys
+        data = Msg.req_keys
+        self.sock.send(data)
+
+        # Wait for keys
+        data = self.sock.recv(buffer_size)
+
+        try:
+            received = json.loads(data)
+            p = received['p']
+            g = received['g']
+        except KeyError:
+            error()
+
+        # Send to server A and wait for B
+        b = randint(0, 100)
+        B = g ** b % p
+        data = Msg.b % B
+        self.sock.send(data)
+
+        data = self.sock.recv(buffer_size)
+        try:
+            received = json.loads(data)
+            A = received['a']
+        except KeyError:
+            error()
+
+        # key
+        self.K = A ** b % p
+
+        # Send info about encryption
+        data = Msg.encr_req % self.encryption
+        sleep(0.1)
+        self.sock.send(data)
+
+        while alive == 1:
+            data = self.sock.recv(buffer_size)
+            try:
+                data = json.loads(data)
+                msg = data['msg']
+                sender = data['from']
+            except ValueError:
+                error()
+                return
+            msg = base64.b64decode(msg)
+            msg = decrypt(msg, self.encryption, self.K)
+            self.textEdit.append('[{}] {}'.format(sender, msg))
 
     def client_send_message(self):
-        ip_address = self.lineEdit.text()
-        port = int(self.lineEdit_2.text())
-
-        nick = self.lineEdit_2.text()
-        nick = nick.replace("#>","")
-        rmessage = self.textEdit.toPlainText()
-        rmessage = rmessage.replace("#>","")
-
-        rmsg = nick + " #> " + rmessage
-
-        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        try:
-            c.connect((ip_address, port))
-        except:
-            msg_box("Connection Refused", "The Address You Are Trying To Reach Is Currently Unavailable.")
-            return
-
-        try:
-            c.send(rmsg)
-            self.listWidget.addItem(rmsg)
-            self.textEdit.setText("")
-        except:
-            msg_box("Connection Refused", "The Message Cannot Be Sent. End-Point Not Connected!")
-
-        c.close()
+        # nick = self.lineEdit_port.text()
+        # rmessage = self.textEdit.toPlainText()
+        # rmessage = rmessage.replace("#>","")
+        #
+        # rmsg = nick + " #> " + rmessage
+        #
+        # c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #
+        # try:
+        #     c.connect((ip_address, port))
+        # except:
+        #     msg_box("Connection Refused", "The Address You Are Trying To Reach Is Currently Unavailable.")
+        #     return
+        #
+        # try:
+        #     c.send(rmsg)
+        #     self.listWidget.addItem(rmsg)
+        #     self.textEdit.setText("")
+        # except:
+        #     msg_box("Connection Refused", "The Message Cannot Be Sent. End-Point Not Connected!")
+        #
+        # c.close()
+        pass
 
 
 if __name__ == "__main__":
@@ -200,5 +272,5 @@ if __name__ == "__main__":
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-
     sys.exit(app.exec_())
+    
